f2b16b009b27e29ee20abfc5081fcf7b35558615
Sidra Jamil <sidrajamil1096@gmail.com>
2026-09-08 15:05:00 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SheetsModule } from './sheets/sheets.module';
-import { GoogleAuthModule } from './google-auth/google-auth.module';
-import { CalendarModule } from './calendar/calendar.module';
+import { CalendarService } from './calendar.service';
+import { CalendarController } from './calendar.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    GoogleAuthModule,
-    CalendarModule,
-    SheetsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule],
+  controllers: [CalendarController],
+  providers: [CalendarService],
+  exports: [CalendarService],
 })
-export class AppModule {}
+export class CalendarModule {}
